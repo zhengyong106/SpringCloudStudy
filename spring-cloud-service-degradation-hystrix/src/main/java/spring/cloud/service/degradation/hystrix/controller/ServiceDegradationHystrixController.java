370a8e262eae6b35a68350c20230b3bf658120d1
zhengyong <1192133596@qq.com>
2019-05-24 18:18:11 +0800

    first commit!

--- spring-cloud-service-degradation-hystrix/src/main/java/spring/cloud/service/degradation/hystrix/controller/ServiceDegradationHystrixController.java
+++ spring-cloud-service-degradation-hystrix/src/main/java/spring/cloud/service/degradation/hystrix/controller/ServiceDegradationHystrixController.java
@@ -1,20 +1,19 @@
-package spring.cloud.service.consume.ribbon.controller;
+package spring.cloud.service.degradation.hystrix.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
-public class ServiceConsumeRibbonController {
+public class ServiceDegradationHystrixController {
     @Autowired
     //注入restTemplate
     private RestTemplate restTemplate;
 
     @GetMapping("/getService")
+    @HystrixCommand(fallbackMethod = "hystrixFallback")
     public String getService(){
         // 当启用Ribbon后直接访问服务提供者接口会报错
         // return restTemplate.getForEntity("http://localhost:8011/serviceDiscovery",String.class).getBody();
@@ -23,11 +22,7 @@ public class ServiceConsumeRibbonController {
         return restTemplate.getForEntity("http://spring-cloud-service-register/serviceDiscovery", String.class).getBody();
     }
 
-    @GetMapping("/getService/{serviceId}")
-    public String getServiceById(@PathVariable("serviceId") String serviceId){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("serviceId", serviceId);
-        // 使用restTemplate调用微服务接口
-        return restTemplate.getForEntity("http://spring-cloud-service-register/serviceDiscovery?serviceId={serviceId}", String.class, uriVariables).getBody();
+    public String hystrixFallback(){
+        return "error";
     }
 }
